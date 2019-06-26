9c0b21c912ecc07dc809ccee6b1699149c2753b7
Daniel Levyson <daniel.levyson@PTGOJEKs-MacBook-Pro-5.local>
2019-06-26 12:48:57 +0700

    create mock cache

---
+++ src/main/kotlin/com/mocked/cache/Cache.kt
@@ -0,0 +1,28 @@
+package com.mocked.cache
+
+const val EXPIRATION_IN_MILLISECONDS = 5000
+
+class Cache {
+    private val weathersByLoction: HashMap<String, CachedWeather?> = HashMap()
+
+    fun save(key: String, weather: Weather?) {
+        if (weather == null) {
+            weathersByLoction[key] = null
+        } else {
+            weathersByLoction[key] = CachedWeather(weather, System.currentTimeMillis())
+        }
+    }
+
+    fun get(key: String) : Weather? {
+        val cachedWeather = weathersByLoction[key] ?: return null
+
+        if ((System.currentTimeMillis() - cachedWeather.createdAt) > EXPIRATION_IN_MILLISECONDS) {
+            save(key, null)
+            return null
+        }
+
+        return cachedWeather.weather
+    }
+
+    private data class CachedWeather(val weather: Weather, val createdAt: Long)
+}
---
+++ src/main/kotlin/com/mocked/cache/Weather.kt
@@ -0,0 +1,3 @@
+package com.mocked.cache
+
+data class Weather(val location: String, val type: String, val temperature: String, val day: String)
