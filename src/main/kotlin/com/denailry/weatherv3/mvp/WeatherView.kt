64e1e2fea5d12c099d2e06b8a81bfb9cf88a36cc
Daniel Levyson <daniel.levyson@PTGOJEKs-MacBook-Pro-5.local>
2019-06-27 11:32:13 +0700

    implement WeatherView to be able to accept location and day as string from the Application

--- src/main/kotlin/com/denailry/weatherv3/mvp/WeatherView.kt
+++ src/main/kotlin/com/denailry/weatherv3/mvp/WeatherView.kt
@@ -3,6 +3,18 @@ package com.denailry.weatherv3.mvp
 import java.io.PrintStream
 
 class WeatherView(private val printer: PrintStream) : WeatherContract.View {
+    constructor() : this(System.out)
+
+    private var presenter: WeatherContract.Presenter? = null
+
+    fun acceptLocationAndDay(location: String, day: String) {
+        presenter?.searchWeathersByLocationAndDay(location, day)
+    }
+
+    fun setPresenter(presenter: WeatherContract.Presenter) {
+        this.presenter = presenter
+    }
+
     override fun showWeathers(weathers: List<WeatherModel>) {
         weathers.forEach { printer.println(it) }
     }
--- src/test/kotlin/com/denailry/weatherv3/mvp/WeatherViewTest.kt
+++ src/test/kotlin/com/denailry/weatherv3/mvp/WeatherViewTest.kt
@@ -8,9 +8,20 @@ import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 
 class WeatherViewTest {
+    private val presenter = object : WeatherContract.Presenter {
+        var lastSearchedLocation: String? = null
+            private set
+        var lastSearchedDay: String? = null
+            private set
+
+        override fun searchWeathersByLocationAndDay(location: String, day: String) {
+            lastSearchedLocation = location
+            lastSearchedDay = day
+        }
+    }
+
     private var output = ByteArrayOutputStream()
     private var printer = PrintStream(output)
-
 
     @Test
     fun `given list of weathers when showWeathers is called then view should print the weather`() {
@@ -34,5 +45,18 @@ class WeatherViewTest {
             }
         }
         assertEquals(0, models.size)
+    }
+
+    @Test
+    fun `given location and day when accepting given data then view should tell presenter to search weathers`() {
+        val location = "jakarta"
+        val day = "monday"
+
+        val view = WeatherView()
+        view.setPresenter(presenter)
+        view.acceptLocationAndDay(location, day)
+
+        assertEquals(location, presenter.lastSearchedLocation)
+        assertEquals(day, presenter.lastSearchedDay)
     }
 }
