adb49eb3676c85cde1acf4b660b7f8ff01e27461
Daniel Levyson <daniel.levyson@PTGOJEKs-MacBook-Pro-5.local>
2019-06-27 15:37:58 +0700

    refactor PersistentStorage to use Mockito instead of creating dummy object from interface

--- src/test/kotlin/com/denailry/weatherv3/repository/PersistentStorageTest.kt
+++ src/test/kotlin/com/denailry/weatherv3/repository/PersistentStorageTest.kt
@@ -4,50 +4,48 @@ import com.denailry.weatherv3.mvp.WeatherModel
 import com.mocked.database.Database
 import com.mocked.database.Weather
 import com.mocked.database.WeatherDay
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.argumentCaptor
+import com.nhaarman.mockitokotlin2.doNothing
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 
 class PersistentStorageTest {
-    private val storage = object : Database {
-        var lastSavedWeather: Weather? = null
-            private set
-        var weathers: List<Weather> = ArrayList()
+    @Mock
+    lateinit var storage: Database
 
-        override fun clean() {}
-
-        override fun save(newWeather: Weather) {
-            lastSavedWeather = newWeather
-        }
-
-        override fun getByLocation(location: String): List<Weather> {
-            return weathers
-        }
-
-        override fun delete(location: String, day: WeatherDay) {
-            lastSavedWeather = Weather(location, day, 25.5f, "")
-        }
+    @BeforeEach
+    fun init() {
+        MockitoAnnotations.initMocks(this)
     }
 
     @Test
     fun `given weather model when saving then saved weather should me identical to weather model`() {
+        val captor = argumentCaptor<Weather>()
+        doNothing().`when`(storage).save(captor.capture())
+
         val database = PersistentStorage(storage)
         val model = WeatherModel("jakarta", WeatherModel.Day.MONDAY, 25.5f, "shiny")
 
         database.create(model)
 
-        assertEquals(model.location, storage.lastSavedWeather?.location)
-        assertEquals(model.day.toString(), storage.lastSavedWeather?.day.toString())
+        assertEquals(model.location, captor.firstValue.location)
+        assertEquals(model.day.toString(), captor.firstValue.day.toString())
     }
 
     @Test
     fun `given location when read is called then return all saved weather models corresponding to location`() {
         val location = "jakarta"
-        val weathers = arrayListOf<Weather>(
+        val weathers = arrayListOf(
             Weather(location, WeatherDay.SUNDAY, 5.0f, "shiny"),
             Weather(location, WeatherDay.MONDAY, 5.0f, "shiny"),
             Weather(location, WeatherDay.TUESDAY, 5.0f, "shiny")
         )
-        storage.weathers = weathers
+        Mockito.`when`(storage.getByLocation(any())).thenReturn(weathers)
 
         val database = PersistentStorage(storage)
         val results = database.read(location)
@@ -67,23 +65,30 @@ class PersistentStorageTest {
 
     @Test
     fun `given weather model when update is called then identical weather corresponding to model should be saved`() {
+        val captor = argumentCaptor<Weather>()
+        doNothing().`when`(storage).save(captor.capture())
+
         val database = PersistentStorage(storage)
         val model = WeatherModel("jakarta", WeatherModel.Day.MONDAY, 25.5f, "shiny")
 
         database.update(model)
 
-        assertEquals(model.location, storage.lastSavedWeather?.location)
-        assertEquals(model.day.toString(), storage.lastSavedWeather?.day.toString())
+        assertEquals(model.location, captor.firstValue.location)
+        assertEquals(model.day.toString(), captor.firstValue.day.toString())
     }
 
     @Test
     fun `given weather model when delete is called then location and day passed should be identical to model`() {
+        val locationCaptor = argumentCaptor<String>()
+        val dayCaptor = argumentCaptor<WeatherDay>()
+        doNothing().`when`(storage).delete(locationCaptor.capture(), dayCaptor.capture())
+
         val database = PersistentStorage(storage)
         val model = WeatherModel("jakarta", WeatherModel.Day.MONDAY, 25.5f, "shiny")
 
         database.delete(model)
 
-        assertEquals(model.location, storage.lastSavedWeather?.location)
-        assertEquals(model.day.toString(), storage.lastSavedWeather?.day.toString())
+        assertEquals(model.location, locationCaptor.firstValue)
+        assertEquals(model.day.toString(), dayCaptor.firstValue.toString())
     }
 }
