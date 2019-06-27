41994f05b6dc4fff735112945f6c4b39aca24fdf
Daniel Levyson <daniel.levyson@PTGOJEKs-MacBook-Pro-5.local>
2019-06-27 23:04:11 +0700

    refactor CacheStorageTest to use Mockito instead of creating dummy implementation of interfaces

--- src/test/kotlin/com/denailry/weatherv3/repository/CacheStorageTest.kt
+++ src/test/kotlin/com/denailry/weatherv3/repository/CacheStorageTest.kt
@@ -3,53 +3,58 @@ package com.denailry.weatherv3.repository
 import com.denailry.weatherv3.mvp.WeatherModel
 import com.mocked.cache.Cache
 import com.mocked.cache.Weather
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.argumentCaptor
+import com.nhaarman.mockitokotlin2.doAnswer
+import com.nhaarman.mockitokotlin2.doNothing
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 
 class CacheStorageTest {
-    private var storage = object : Cache {
-        var lastKey: String = ""
-            private set
-        var lastWeather: Weather? = Weather("", "", "", "")
-            private set
-        var savedWeathers = HashMap<String, Weather>()
+    @Mock
+    lateinit var storage: Cache
 
-        override fun save(key: String, weather: Weather?) {
-            lastKey = key
-            lastWeather = weather
-        }
-
-        override fun get(key: String): Weather? {
-            return savedWeathers[key]
-        }
-
-        fun setSavedWeathers(weathers: List<Weather>) {
-            savedWeathers = HashMap()
-            weathers.forEach { savedWeathers["${it.location}:${it.day}"] = it }
-        }
+    @BeforeEach
+    fun init() {
+        MockitoAnnotations.initMocks(this)
     }
 
     @Test
     fun `given weather when saving then key should be composed from location and day`() {
+        val keyCaptor = argumentCaptor<String>()
+        val weatherCaptor = argumentCaptor<Weather>()
+        doNothing().`when`(storage).save(keyCaptor.capture(), weatherCaptor.capture())
+
         val cache = CacheStorage(storage)
         val model = WeatherModel("jakarta", WeatherModel.Day.MONDAY, 25.5f, "shiny")
 
         cache.create(model)
 
-        val expected = model.location + ":" + model.day.toString()
-
-        assertEquals(expected, storage.lastKey)
+        assertEquals("${model.location}:${model.day}", keyCaptor.firstValue)
+        assertEquals(model.location, weatherCaptor.firstValue.location)
+        assertEquals(model.day.toString(), weatherCaptor.firstValue.day)
     }
 
     @Test
     fun `given location when read is called then return all saved weathers of that location no less or more`() {
         val location = "jakarta"
-        val weathers = arrayListOf<Weather>(
+        val weathers = arrayListOf(
             Weather(location, "x", "0.0", "monday"),
             Weather(location, "x", "0.0", "friday"),
             Weather(location, "x", "0.0", "tuesday")
         )
-        storage.setSavedWeathers(weathers)
+
+        doAnswer {
+            when {
+                it.arguments[0].toString() == "$location:${weathers[0].day}" -> return@doAnswer weathers[0]
+                it.arguments[0].toString() == "$location:${weathers[1].day}" -> return@doAnswer weathers[1]
+                it.arguments[0].toString() == "$location:${weathers[2].day}" -> return@doAnswer weathers[2]
+            }
+            return@doAnswer null
+        }.`when`(storage).get(any())
 
         val cache = CacheStorage(storage)
         val results = cache.read(location)
@@ -69,24 +74,32 @@ class CacheStorageTest {
 
     @Test
     fun `given weather when delete is called then null should be saved on corresponding location-day pair`() {
+        val keyCaptor = argumentCaptor<String>()
+        val weatherCaptor = argumentCaptor<Weather>()
+        doNothing().`when`(storage).save(keyCaptor.capture(), weatherCaptor.capture())
+
         val model = WeatherModel("jakarta", WeatherModel.Day.MONDAY, 25.5f, "shiny")
         val cache = CacheStorage(storage)
 
         cache.delete(model)
 
-        assertEquals("${model.location}:${model.day}", storage.lastKey)
-        assertNull(storage.lastWeather)
+        assertEquals("${model.location}:${model.day}", keyCaptor.firstValue)
+        assertNull(weatherCaptor.firstValue)
     }
 
     @Test
     fun `given weather when update is called then weather and corresponding key should be saved to storage`() {
+        val keyCaptor = argumentCaptor<String>()
+        val weatherCaptor = argumentCaptor<Weather>()
+        doNothing().`when`(storage).save(keyCaptor.capture(), weatherCaptor.capture())
+
         val model = WeatherModel("jakarta", WeatherModel.Day.MONDAY, 25.5f, "shiny")
         val cache = CacheStorage(storage)
 
         cache.update(model)
 
-        assertEquals("${model.location}:${model.day}", storage.lastKey)
-        assertEquals(model.location, storage.lastWeather?.location)
-        assertEquals(model.day.toString(), storage.lastWeather?.day)
+        assertEquals("${model.location}:${model.day}", keyCaptor.firstValue)
+        assertEquals(model.location, weatherCaptor.firstValue.location)
+        assertEquals(model.day.toString(), weatherCaptor.firstValue.day)
     }
 }
