91399b9b5ac1b22d9cb8457a626a46cfba0a067c
Daniel Levyson <daniel.levyson@PTGOJEKs-MacBook-Pro-5.local>
2019-06-27 11:54:36 +0700

    implement filter in WeatherPresenter when searching weathers to only return maximum 5 weathers

--- src/main/kotlin/com/denailry/weatherv3/mvp/WeatherPresenter.kt
+++ src/main/kotlin/com/denailry/weatherv3/mvp/WeatherPresenter.kt
@@ -1,10 +1,59 @@
 package com.denailry.weatherv3.mvp
 
 import com.denailry.weatherv3.repository.Repository
+import kotlin.math.max
+import kotlin.math.min
 
 class WeatherPresenter(private val view: WeatherContract.View, private val repo: Repository) : WeatherContract.Presenter {
+    companion object {
+        const val MAX_SHOWED_WEATHER = 5
+    }
+
     override fun searchWeathersByLocationAndDay(location: String, day: String) {
         val weathers = repo.read(location)
-        view.showWeathers(weathers)
+        view.showWeathers(filterWeatherByDay(weathers, day))
+    }
+
+    private fun filterWeatherByDay(weathers: List<WeatherModel>, day: String) : List<WeatherModel> {
+        weathers.sortedWith(compareBy{it.day})
+
+        val startIndex = findIndexOfDay(weathers, day) ?: return ArrayList()
+
+        val results = ArrayList<WeatherModel>()
+        val maxItem = min(MAX_SHOWED_WEATHER, weathers.size)
+
+        var i = startIndex
+        while (results.size < maxItem) {
+            results.add(weathers[i])
+            i = (i + 1) % weathers.size
+        }
+
+        return results
+    }
+
+    private fun findIndexOfDay(weathers: List<WeatherModel>, rawDay: String) : Int? {
+        val day = parseDay(rawDay) ?: null
+
+        for ((index, weather) in weathers.withIndex()) {
+            if (weather.day == day) {
+                return index
+            }
+        }
+
+        return null
+    }
+
+    private fun parseDay(day: String) : WeatherModel.Day? {
+        when (day) {
+            "monday" -> return WeatherModel.Day.MONDAY
+            "tuesday" -> return WeatherModel.Day.TUESDAY
+            "wednesday" -> return WeatherModel.Day.WEDNESDAY
+            "thursday" -> return WeatherModel.Day.THURSDAY
+            "friday" -> return WeatherModel.Day.FRIDAY
+            "saturday" -> return WeatherModel.Day.SATURDAY
+            "sunday" -> return WeatherModel.Day.SUNDAY
+        }
+
+        return null
     }
 }
--- src/test/kotlin/com/denailry/weatherv3/mvp/WeatherPresenterTest.kt
+++ src/test/kotlin/com/denailry/weatherv3/mvp/WeatherPresenterTest.kt
@@ -3,6 +3,7 @@ package com.denailry.weatherv3.mvp
 import com.denailry.weatherv3.repository.Repository
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.time.MonthDay
 
 class WeatherPresenterTest {
     private val view = object : WeatherContract.View {
@@ -51,5 +52,31 @@ class WeatherPresenterTest {
             }
         }
         assertEquals(0, models.size)
+    }
+
+    @Test
+    fun `given location and day when searching by given data then give only 5 consecutive day of weather to view`() {
+        val location = "jakarta"
+        val models : ArrayList<WeatherModel> = arrayListOf(
+            WeatherModel(location, WeatherModel.Day.MONDAY, 25.0f, "shiny"),
+            WeatherModel(location, WeatherModel.Day.TUESDAY, 25.0f, "rainy"),
+            WeatherModel(location, WeatherModel.Day.WEDNESDAY, 25.0f, "rainy"),
+            WeatherModel(location, WeatherModel.Day.THURSDAY, 25.0f, "rainy"),
+            WeatherModel(location, WeatherModel.Day.FRIDAY, 25.0f, "rainy"),
+            WeatherModel(location, WeatherModel.Day.SATURDAY, 25.0f, "rainy"),
+            WeatherModel(location, WeatherModel.Day.SUNDAY, 25.0f, "rainy")
+        )
+        repository.savedWeathers = models
+
+        val presenter = WeatherPresenter(view, repository)
+        presenter.searchWeathersByLocationAndDay("jakarta", "monday")
+
+        val showedWeathers = view.showedWeathers
+        assertEquals(5, showedWeathers.size)
+        assertEquals(WeatherModel.Day.MONDAY, showedWeathers[0].day)
+        assertEquals(WeatherModel.Day.TUESDAY, showedWeathers[1].day)
+        assertEquals(WeatherModel.Day.WEDNESDAY, showedWeathers[2].day)
+        assertEquals(WeatherModel.Day.THURSDAY, showedWeathers[3].day)
+        assertEquals(WeatherModel.Day.FRIDAY, showedWeathers[4].day)
     }
 }
