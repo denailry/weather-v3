7604b9d8133cad4e54b896bb53a26cf507aff4f3
Daniel Levyson <daniel.levyson@PTGOJEKs-MacBook-Pro-5.local>
2019-06-27 12:18:17 +0700

    create Application to setup the view, presenter, repository, and data source

--- src/main/kotlin/com/denailry/weatherv3/Application.kt
+++ src/main/kotlin/com/denailry/weatherv3/Application.kt
@@ -1,5 +1,45 @@
 package com.denailry.weatherv3
 
+import com.denailry.weatherv3.mvp.WeatherContract
+import com.denailry.weatherv3.mvp.WeatherPresenter
+import com.denailry.weatherv3.mvp.WeatherView
+import com.denailry.weatherv3.repository.PersistentStorage
+import com.mocked.database.GoSQL
+
+class Application {
+    private var view: WeatherView? = null
+
+    fun run() {
+        view = WeatherView()
+        val database = GoSQL("src/main/resources/weathers.txt")
+        val repository = PersistentStorage(database)
+        val presenter = WeatherPresenter(view!!, repository)
+        view!!.setPresenter(presenter)
+        startReceivingInput()
+    }
+
+    private fun startReceivingInput() {
+        val inputHandler = view ?: return
+        var input: String? = null
+
+        while (input != "Q") {
+            if (input != null) {
+                handleCommands(inputHandler, input.split(" "))
+            }
+            input = readLine()
+        }
+    }
+
+    private fun handleCommands(handler: WeatherView, commands: List<String>) {
+        if (commands.size != 2) {
+            println("-- invalid input --")
+        } else {
+            handler.acceptLocationAndDay(commands[0], commands[1])
+        }
+    }
+}
+
 fun main() {
-    println("Weather App has been initialized!")
+    val app = Application()
+    app.run()
 }
